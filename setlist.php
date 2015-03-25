932dfb682fd9af0a6604fbfbc51c73a7b0a3606a
jlwalm <jeremy.walmsley@gmail.com>
2015-03-25 14:52:20 +0000

    Update setlist.php

--- setlist.php
+++ setlist.php
@@ -206,7 +206,7 @@ $result_first = mysql_query("select * from songs where idsongs < '0' order by so
 echo "<table class='table table-striped table-bordered table-condensed table-hover' border='0'><tr><td><form method='GET' action='" . $php_self . "'>
 
 Add Song: 
-<select name='idsongs' id='idsongs'>";
+<select name='idsongs' id='idsongs'>"<optgroup>;
 
 //loop through results and populate dropdown box
 
@@ -215,7 +215,7 @@ Add Song:
   echo "<option value='" . $row['idsongs'] . "'>" . $row['song_name'] . "</option>";
   }
 
-
+echo "</optgroup>";
  while ($row = mysql_fetch_array($result))
  {
   echo "<option value='" . $row['idsongs'] . "'>" . $row['song_name'] . "</option>";
